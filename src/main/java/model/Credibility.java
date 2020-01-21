0b8fe9e9edff7bc5ecc2071e28a4a6e4d4a7e3f8
Andreas <andreas.j.berg@gmail.com>
2020-01-21 17:27:36 +0100

    json formatting fix

--- src/main/java/model/Credibility.java
+++ src/main/java/model/Credibility.java
@@ -1,18 +1,10 @@
 package model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public enum Credibility {
-    @JsonProperty("not verifiable")
     not_verifiable_post,
-    @JsonProperty("not credible")
     not_credible_post,
-    @JsonProperty("mostly not credible")
     mostly_not_credible_post,
-    @JsonProperty("credibility uncertain")
     credible_uncertain_post,
-    @JsonProperty("mostly credible")
     mostly_credible_post,
-    @JsonProperty("credible")
     credible_post;
 }
